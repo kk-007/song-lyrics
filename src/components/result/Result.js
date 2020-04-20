c2366eb2631ec229d7466bac3539ba22bf084662
Keval navadiya <31183263+kk-007@users.noreply.github.com>
2020-04-20 22:46:41 +0530

    Updated Backend

--- src/components/result/Result.js
+++ src/components/result/Result.js
@@ -1,22 +1,31 @@
-import React,{useState,useEffect} from 'react'
+import React, { useState, useEffect } from 'react';
 
 function Result(props) {
-    const [lyrics,setLyrics] = useState("");
-    useEffect(()=>{
-        // console.log(`https://no-cors-fetch.herokuapp.com/getJson?url=https://api.lyrics.ovh/v1/${props.lyrics.artist}/${props.lyrics.title}`);
-        fetch(`https://no-cors-fetch.herokuapp.com/getJson?url=https://api.lyrics.ovh/v1/${props.lyrics.artist}/${props.lyrics.title}`)
-        .then(data=>data.json())
-        .then(result=>{
-            result.error?setLyrics(result.error):setLyrics(result.lyrics)
-        });
-    },[props.lyrics.title,props.lyrics.artist]);
-    return (
-        <div>
-            {props.lyrics.title?props.lyrics.title+"-"+props.lyrics.artist:''}
-            <br/>
-            {props.lyrics.title?lyrics:''}
-        </div>
-    )
+	const [ lyrics, setLyrics ] = useState('');
+	useEffect(
+		() => {
+			// console.log(`https://no-cors-fetch.herokuapp.com/getJson?url=https://api.lyrics.ovh/v1/${props.lyrics.artist}/${props.lyrics.title}`);
+			fetch(
+				`https://no-cors-fetch.herokuapp.com/getJson?url=https://api.lyrics.ovh/v1/${props.lyrics
+					.artist}/${props.lyrics.title}`
+			)
+				.then((data) => data.json())
+				.then((result) => {
+					result.error ? setLyrics(result.error) : setLyrics(result.lyrics);
+				})
+				.catch((error) => {
+					console.error('Error:', error);
+				});
+		},
+		[ props.lyrics.title, props.lyrics.artist ]
+	);
+	return (
+		<div>
+			{props.lyrics.title ? props.lyrics.title + '-' + props.lyrics.artist : ''}
+			<br />
+			{props.lyrics.title ? lyrics : ''}
+		</div>
+	);
 }
 
 export default Result;
