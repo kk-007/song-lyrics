e5f27d2957c389d2344da10159eb71d050fcf2ef
Keval navadiya <31183263+kk-007@users.noreply.github.com>
2020-04-27 21:02:37 +0530

    finished backend........

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
-import React,{useState} from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Search from './components/search/Search';
 import Result from './components/result/Result';
 function App() {
-  const [lyrics,setLyrics] = useState({});
-  let displayLyrics = (artist,title)=>{
-    // console.log(artist,title);
-    setLyrics({artist,title});
-  }
-  return (
-    <div>
-      <Search displayLyrics={displayLyrics}/>
-      <Result lyrics={lyrics}/>
-    </div>
-  );
+	const [ lyrics, setLyrics ] = useState({});
+	let displayLyrics = (artist, title) => {
+		// console.log(artist,title);
+		setLyrics({ artist, title });
+	};
+	return (
+		<div className="container">
+			<div>
+				<Search displayLyrics={displayLyrics} />
+			</div>
+			<div>
+				<Result lyrics={lyrics} />
+			</div>
+		</div>
+	);
 }
 
 export default App;
