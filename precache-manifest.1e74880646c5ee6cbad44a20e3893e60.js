38dca2647ac4563e072442a85de4ec2cf64da5de
Keval navadiya <31183263+kk-007@users.noreply.github.com>
2020-04-27 23:13:10 +0530

    Updates

---
+++ precache-manifest.1e74880646c5ee6cbad44a20e3893e60.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae6ce0f070602081895b5583814738e5",
+    "url": "/song-lyrics/index.html"
+  },
+  {
+    "revision": "a571908529e1110c1d2f",
+    "url": "/song-lyrics/static/css/main.15f5a55c.chunk.css"
+  },
+  {
+    "revision": "dd44819549dcaaa01332",
+    "url": "/song-lyrics/static/js/2.92c43ee1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/song-lyrics/static/js/2.92c43ee1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a571908529e1110c1d2f",
+    "url": "/song-lyrics/static/js/main.68a2fbe5.chunk.js"
+  },
+  {
+    "revision": "f51c1a714bb8f8127837",
+    "url": "/song-lyrics/static/js/runtime-main.ee22e8be.js"
+  },
+  {
+    "revision": "70ff2694f82da434d5b19226e7cfc528",
+    "url": "/song-lyrics/static/media/logo.70ff2694.svg"
+  }
+]);
